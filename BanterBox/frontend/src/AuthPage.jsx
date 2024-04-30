ba98cb7ee08a9037324b9997af40de697367ee5d
Chandan1293 <chandanvallabhaneni@gmail.com>
2024-04-30 13:57:48 +0530

    commit

--- BanterBox/frontend/src/AuthPage.jsx
+++ BanterBox/frontend/src/AuthPage.jsx
@@ -3,7 +3,7 @@ const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
-      axios.post("http://localhost:3001/authenticate", { username: value})
+      axios.post("https://chandan1293-github-io-3.onrender.com/authenticate", { username: value})
       .then(r => props.onAuth({ ...r.data, secret: value }))
         .catch(e => console.log("error",e));
     };
